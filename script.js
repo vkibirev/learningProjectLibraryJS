24a1c20f6002b912a1539252bac1bc53d3808eec
kibirev (Виктор Кибирев) <kibirev@office.local>
2018-07-10 13:19:03 +0300

    refactored adding feature

--- script.js
+++ script.js
@@ -2,7 +2,17 @@ const readLine = require('readline-sync');
 const fs = require('fs');
 const sinon = require('sinon');
 
-// const myQuestion = sinon.stub(readLine, 'question');
+const myQuestion = sinon.stub(readLine, 'question');
+const baseScenario = [
+    'Clear', 'List All', 'Add', 'Author', '', 'test', '', 'en', 'drama', 'Back', 'List All', 
+    'Add', 'Author', 'test2', 'ru', 'comedy', 'Back', 'List All', 
+    'Add', 'Book', '', 'bookTitle1', 'test3', '', 'test', 'Back', 'List All',
+    'Exit', 
+];
+
+for (let i = 0; i < baseScenario.length; ++i) {
+    myQuestion.onCall(i).returns(baseScenario[i]);
+}
 
 //Function that save data into JSON 
 let saveData = function (data, filePath) {
@@ -17,8 +27,8 @@ let readFile = function (filePath) {
 
 const db = 
 {
-    books: readFile('books.json'),
-    authors: readFile('authors.json')
+    books: readFile('books.json'), 
+    authors: readFile('authors.json'),
 };
 
 //Search function, uses for comparing data from JOSN and cmd
@@ -30,113 +40,72 @@ let foundAuthor = function (data, value) {
     }
 }
 
-//Function for adding authors and books into JSON
-let adder = function(type, filePath) {   
-    
-    switch(type) {        
-        case 'author':        
-            let tempAuthors = {}; //temprorary object that is needed for adding author information
-            console.log('Adding new author');
-            //Check if autor name field isn't empty and author doesn't exist
-            while(true){
-                let authorName = readLine.question('Name: ');
+function isValid(val, validators) {
+    for (let i in validators) {
+        let validator = validators[i];
+        let name = validator.name;
+        let props = validator.props;
 
-                if (authorName === null || authorName === '') {    
-                    console.log(`Field can't be empty`);
-                    continue;
-                } 
-                if (foundAuthor(db['authors'], authorName)) {
-                    console.log('Author already exists. Please add newone\n');
-                    continue;                        
+        switch(name) {
+            case 'required':
+                if(props.value && !val) {                   
+                    return false;
                 }
-                tempAuthors.name = authorName; 
                 break;
-            }            
-            // Check if Language field is filled
-            while (true) {
-                let authorLanguage = readLine.question('Language: ');
-                if (authorLanguage === null || authorLanguage === '') {
-                    console.log(`Field can't be empty`);
-                    continue;
+            case 'unique':
+                for (let i in props.among) {
+                    let item = props.among[i];
+                    if(item[props.field] === val) {                      
+                        return false;
+                    }
                 }
-                tempAuthors.lang = authorLanguage;
                 break;
-            }
-            //Check if Genre field is filled
-            while(true) {
-                let authorGenre = readLine.question('Genre: ');
-                if (authorGenre === null || authorGenre === '') {
-                    console.log(`Field can't be empty`);
-                    continue;
+            case 'exists':
+                for (let i in props.among) {
+                    let item = props.among[i];
+                    if(item[props.field] === val) {
+                        return true;
+                    }                 
                 }
-                tempAuthors.genre = authorGenre;
-                break;
-            }
-            db['authors'].push(tempAuthors); //add object with new author into array of objects with all previously added authors
-            saveData(db['authors'], 'authors.json'); //save array of objects with authors into JSON
-            add();         
             break;
-        case 'book':
-        default:
-            let tempBooks = {}; //temprorary object that nis needed for adding books information
-
-            let bookTitle = readLine.question('Title: ');
-            tempBooks.bookTitle = bookTitle;
-            //Check if aouthor that should be used for adding book is presented in the authors.json
-            while(true) {
-                let bookAuthor = readLine.question('Author: ');
-                if (foundAuthor(db['authors'], bookAuthor)) {                    
-                    tempBooks.author = bookAuthor;
-                    break;
-                }
-                console.log('Incorrect author\n');
-            }
-            tempBooks.rate = 0; //Add default zero rate for each book
-            db['books'].push(tempBooks);
-            saveData(db['books'], 'books.json');
-            add();
-            break;
+        }
     }
+    return true;
 }
 
-//Function that displayed main menu and call functions for each menu item
-let mainMenu = function() {
-    
-    console.log('\nLibrary\n');    
-    console.log(`1: List all\n2: Search\n3: Add\n4: Rate\n5: Clear\n6: Reload\n`);    
+function getTempData(schema) {
 
-    let selectedItem = readLine.question();
-    switch(selectedItem) {
-        case '1':
-        case 'List All': 
-            listAll();
-            break;
-        case '2':
-        case 'Search':
-            search();
-            break;
-        case '3':
-        case 'Add':
-            add();
-            break;
-        case '4':
-        case 'Rate':
-            rate();
-            break;
-        case '5':
-        case 'Clear':
-            clear();
-            break;
-        case '6':
-        case 'Reload':
-            reload();
-            break;
-        default: 
-            console.log('Incorrect item had been chosen');
-            mainMenu();
-            break;            
+    let temp = {};
+    for (let field in schema) {
+        console.log(field[0].toUpperCase() + field.slice(1) + ': ');        
+        while (true) {
+            let value = readLine.question();   
+            
+            if (isValid(value, schema[field].validators)) {
+                temp[field] = value;
+                break;
+            }
+            console.log(`${field} is incorrect!`);            
+        }
     }
-};
+    return temp;
+}
+
+let addAuthor = function() {
+    console.log("Adding new author: ");     
+    let schema = schemas.author;
+    let temp = getTempData(schema);
+    db['authors'].push(temp);
+}
+
+let addBook =  function() {
+    console.log("Adding new book: ");     
+    let schema = schemas.book;
+    let temp = getTempData(schema);
+    temp.rate = 0; //Add default zero rate for each book
+    db['books'].push(temp);
+}
+
 
 //Function that is displayed all added authors and books
 let listAll = function() {    
@@ -146,9 +115,8 @@ let listAll = function() {
     }
     console.log('\nBooks: \n');
     for (let i = 0; i < db['books'].length; ++i) {
-        console.log(`${i+1}: ${db['books'][i].bookTitle}(${db['books'][i].author}). Rating: ${db['books'][i].rate}`);        
-    }
-    mainMenu();    
+        console.log(`${i+1}: ${db['books'][i].title}(${db['books'][i].author}). Rating: ${db['books'][i].rate}`);        
+    }   
 };
 
 let search = function() {
@@ -263,33 +231,6 @@ let search = function() {
     }
 }
 
-//Menu "Add" function
-let add = function() {
-    console.log('\nAdd entity\n');
-    console.log(`1: Author\n2: Book\n3: Back\n`);
-
-    let author = {};
-    let selectedItem = readLine.question();
-    switch (selectedItem) {
-        case '1':
-        case 'Author':
-            adder('author');
-            break;
-        case '2':
-        case 'Book':
-            adder('book');;
-            break;
-        case '3':        
-        case 'Back':
-            mainMenu();
-            break;
-        default:
-            console.log('Incorrect item had been chosen');
-            add();
-            break;        
-    }
-    
-}
 //Menu "Rate" function
 let rate = function() {  
     let bookId = readLine.question('\nBook ID: \n');
@@ -309,16 +250,111 @@ let rate = function() {
 
 //Menu "Clear" function
 let clear = function() {
-    let clearData = [];
+    db['books'] = [];
+    db['authors'] = [];
     console.log('\nLibrary is empty now\n');
-    saveData(clearData, 'authors.json');
-    saveData(clearData, 'books.json');
-    mainMenu();    
+    saveData(db['books'], 'authors.json');
+    saveData(db['books'], 'books.json');    
 }
 
 //Function that clear curent console window and call mainMenu
 let reload = function() {
     process.stdout.write('\x1Bc');
-    mainMenu();
 }
-mainMenu();
+
+let endInterract = function() {
+    saveData(db['books'], 'books.json');
+    saveData(db['authors'], 'authors.json');
+    return 'break';
+}
+
+function interact(menu) {
+    while (true) {
+        printMenu(menu)
+        let choice = askForChoice(menu.options)
+        let result = performChoice(choice)
+        if (result === 'break') {
+            break;
+        }
+    }    
+}
+
+function printMenu(menu) {
+    console.log(menu.label);
+    console.log();    
+    for(let i = 0; i < menu.options.length; ++i) {
+        console.log(i+1+'.', menu.options[i].label);        
+    }
+    console.log('-'.repeat(20));    
+}
+
+function askForChoice(menu) {
+    while(true) {
+        let userChoice = readLine.question('Select menu option: ');
+        let option = menu[userChoice - 1];
+
+        if (option) {
+            return option;
+        }
+        for (let i = 0; i < menu.length; ++i) {
+            if (menu[i].label === userChoice) {
+                return menu[i];
+            }
+        }
+        console.log(`Incorrect choice: ${userChoice}`);        
+    }
+}
+
+function performChoice(choice) {
+    return choice.action();
+}
+
+const menus = {
+    main: {
+        label: 'Main menu',
+        options: [
+            {label: 'List All', action: listAll},
+            {label: 'Search', action: search},
+            {label: 'Add', action: (function add() {interact(menus.add)})},
+            {label: 'Rate', action: rate},
+            {label: 'Clear', action: clear},
+            {label: 'Reload', action: reload},
+            {label: 'Exit', action: endInterract},
+        ]
+    },
+
+    add: {
+        label: 'Create entity',
+        options: [
+            {label: 'Author', action: addAuthor},
+            {label: 'Book', action: addBook},
+            {label: 'Back', action: endInterract},
+        ]
+    }
+}
+
+const schemas = {
+    author: {
+        name: {validators: [
+            {name: 'required', props: {value: true}}, 
+            {name: 'unique', props: {field: 'name', among: db['authors']}}
+        ]},
+        lang: {validators: [
+            {name: 'required', props: {value: true}}
+        ]},
+        genre: {validators: [
+            {name: 'required', props: {value: true}}
+        ]},
+    },
+    book: {
+        title: {validators: [
+            {name: 'required', props: {value: true}},
+        ]},
+        author: {validators: [
+            {name: 'required', props: {value: true}},
+            {name: 'exists', props: {among: db['authors'], field: "name"}}
+        ]},
+    },    
+};
+
+interact(menus.main);
